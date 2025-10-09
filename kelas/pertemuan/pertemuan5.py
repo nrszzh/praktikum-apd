eb06de4c0d24cb222e9b53ec67c7accc1be10305
Nur Azizah Islamiyah <nrislamiyah14@gmail.com>
2025-10-09 19:24:48 +0800

    pertemuan5

---
+++ kelas/pertemuan/pertemuan5.py
@@ -0,0 +1,63 @@
+praktikum = ['APD', 'orsikom', 'jarkom', 'RPL']
+
+
+# praktikum.remove('APD')
+# del praktikum[2] #delete
+# buang = praktikum.pop() #pop yg index nya kosong kehapus list blakang
+# print(buang)
+
+# print(praktikum)
+
+# angka = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# huruf = ['a', 'b', 'c']
+# print(angka[0:10:2]) #klo isi nya satu jdi stop, klo 2 jdi start dan stop
+
+# inputNilai = input('Matkul : ')
+
+# praktikum.append(inputNilai)
+# print(praktikum)
+
+# praktikum[2] = 'APD'
+# praktikum.append('Basdat') #nambah belakang
+# praktikum.insert(2, 'strukdat') #sisipkan
+# print(praktikum)
+
+# print(praktikum[1][1])
+
+
+# print(praktikum)
+# for i in praktikum:
+#     print(i)
+
+
+#      indeks  [0][0]   [0][1]   [0][2]        [1][0]      [1][0]  [1][2]
+# mahasiswa = [["Daffa", "Dante", "Santoso"], ["Pernanda", "Triya", "Ahnaf"]]
+# # perulangan for untuk mendapatkan semua elemen
+# for i in mahasiswa:
+#     for j in i :
+#         print (j)
+#         #[0][2]
+
+# mahasiswa = [
+#             ["Daffa", "Dante", "Santoso"], ["Pernanda", "Triya", "Ahnaf"]
+#             ['yoga', 'denny']
+#             ]
+# # perulangan for untuk mendapatkan semua elemen
+# for i in mahasiswa:
+#     for j in i :
+#         print (j)
+#         #[0][2]
+
+
+# tuple = ('apd') #ini bukan tuple, ini termasuk str krna tuple lebih dri satu
+
+# aslab = ['harahap', 'deny']
+# matkul = ('apd', 'matdis')
+# listmatkul = list(matkul)
+# kombinasi = aslab + listmatkul
+# print(kombinasi)
+
+tugas = ('yoga', 'arduino','scrapping', 'matriks', 'visualisasi')
+# beri variabel setiap values
+(PTI, orsikom, basisdata) = tugas
+print()
